df185a6ec8dd7c39dcaa08d58bd9ccd595f64fd1
Joao Vale <joaovale12@gmail.com>
2025-08-25 19:05:17 +0100

    added background, icon and styles to hompage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-dark-200 text-5xl">Welcome</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute z-0 inset-0 w-full h-full" />
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
